91a99126c7ae71a4dce81a025fe4d7ea1dc97bcc
Norayr <asoyannorayr99@gmail.com>
2023-12-11 16:52:14 +0300

    add commit

--- src/ComplexNumber.java
+++ src/ComplexNumber.java
@@ -1,6 +1,12 @@
-import java.util.Objects;
-
 public class ComplexNumber {
+    public static void main(String[] args) {
+        ComplexNumber num1 = new ComplexNumber(2.0, 3.0);
+        ComplexNumber num2 = new ComplexNumber(2.0, 3.0);
+        ComplexNumber num3 = new ComplexNumber(4.0, 5.0);
+
+        System.out.println("num1 equals num2: " + num1.equals(num2));
+        System.out.println("num1 equals num3: " + num1.equals(num3));
+    }
     private double re;
     private double im;
 
@@ -11,8 +17,10 @@ public class ComplexNumber {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
         ComplexNumber that = (ComplexNumber) o;
         return Double.compare(that.re, re) == 0 &&
                 Double.compare(that.im, im) == 0;
@@ -20,15 +28,13 @@ public class ComplexNumber {
 
     @Override
     public int hashCode() {
-        return Objects.hash(re, im);
-    }
-    public static void main(String[] args) {
-        ComplexNumber num1 = new ComplexNumber(2.0, 3.0);
-        ComplexNumber num2 = new ComplexNumber(2.0, 3.0);
-        ComplexNumber num3 = new ComplexNumber(4.0, 5.0);
-
-        System.out.println("num1 equals num2: " + num1.equals(num2));
-        System.out.println("num1 equals num3: " + num1.equals(num3));
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(re);
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(im);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
     }
 }
 
