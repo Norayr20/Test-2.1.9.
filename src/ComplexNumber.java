bf1a9538ad6411aecdb26998137e7793818d4550
Norayr <asoyannorayr99@gmail.com>
2023-12-18 16:42:16 +0300

    add commit

--- src/ComplexNumber.java
+++ src/ComplexNumber.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class ComplexNumber {
     public static void main(String[] args) {
         ComplexNumber num1 = new ComplexNumber(2.0, 3.0);
@@ -7,12 +9,20 @@ public class ComplexNumber {
         System.out.println("num1 equals num2: " + num1.equals(num2));
         System.out.println("num1 equals num3: " + num1.equals(num3));
     }
-    private double re;
-    private double im;
+    private final double re;
+    private final double im;
 
     public ComplexNumber(double re, double im) {
         this.re = re;
         this.im = im;
+    }
+
+    public double getRe() {
+        return re;
+    }
+
+    public double getIm() {
+        return im;
     }
 
     @Override
@@ -21,20 +31,17 @@ public class ComplexNumber {
             return true;
         if (o == null || getClass() != o.getClass())
             return false;
+
         ComplexNumber that = (ComplexNumber) o;
-        return Double.compare(that.re, re) == 0 &&
-                Double.compare(that.im, im) == 0;
+
+        if (Double.compare(that.re, re) != 0)
+            return false;
+        return Double.compare(that.im, im) == 0;
     }
 
     @Override
     public int hashCode() {
-        int result;
-        long temp;
-        temp = Double.doubleToLongBits(re);
-        result = (int) (temp ^ (temp >>> 32));
-        temp = Double.doubleToLongBits(im);
-        result = 31 * result + (int) (temp ^ (temp >>> 32));
-        return result;
+        return Objects.hash(re, im);
     }
 }
 
